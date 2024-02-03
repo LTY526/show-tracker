9ec5fb559018bf4d0a77d78458f747e9788b82d8
Lai Ting Yang <zzlolzzlol88@gmail.com>
2024-02-03 11:12:11 +0800

    update

--- app/login/page.tsx
+++ app/login/page.tsx
@@ -8,7 +8,6 @@ export const metadata: Metadata = {
 export default async function Page() {
   return (
     <>
-      <div className="mx-auto flex min-h-screen w-full flex-col  justify-center gap-5 px-8 md:w-[500px] md:px-5"></div>
       <LoginForm />
     </>
   );
